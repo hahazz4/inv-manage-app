344a71bd5c2ba809da7e0c13caff2a09e64b29c3
Zeerak Asim <Z_zee09@hotmail.com>
2024-08-04 05:49:41 -0400

    added changes, still working on styling and few other stuff.....

--- app/page.js
+++ app/page.js
@@ -2,16 +2,21 @@
 import Image from "next/image";
 // import styles from "./page.module.css";
 import { useState, useEffect } from "react";
-import { firestore, Firestore } from "@/firebase";
-import { collection, getDocs, query } from "firebase/firestore";
-import { Box, Modal, Stack, TextField, Typography } from "@mui/material";
+import { firestore } from "@/firebase";
+import { collection, getDocs, getDoc, query, doc, setDoc, deleteDoc } from "firebase/firestore";
+import { Box, Button, Modal, Stack, TextField, Typography } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import Alert from '@mui/material/Alert';
+import CheckIcon from '@mui/icons-material/Check';
+import { Add } from "@mui/icons-material";
 
 export default function Home(){
   //Setting state variables
   const [inv, setInv] = useState([]);
-  const [open, setOpen] = useState([false]);
+  const [open, setOpen] = useState(false);
   const [itemName, setItemName] = useState('');
-  
+  const [showAlert, setShowAlert] = useState(false);
+
   const updateInv = async() => {
     const snapshot = query(collection(firestore, 'inv'));
     const docs = await getDocs(snapshot);
@@ -28,21 +33,28 @@ export default function Home(){
 
   //Add item
   const addItem = async(item) => {
+    if (item === null || item === '')
+      return;
     const docRefer = doc(collection(firestore, 'inv'), item);
-    const docSnapshot = await getDocs(docRefer);
+    const docSnapshot = await getDoc(docRefer);
     if (docSnapshot.exists()){
       const {quantity} = docSnapshot.data()
       await setDoc(docRefer, {quantity: quantity + 1})
+      setShowAlert(true);
+      <Alert icon={<CheckIcon fontSize="inherit" />} severity="success">
+        Here is a gentle confirmation that your action was successful.
+    </Alert>
     }
     else
       await setDoc(docRefer, {quantity: 1})
     await updateInv();
+    
   };
 
   //Remove Item
   const removeItem = async(item) => {
     const docRefer = doc(collection(firestore, 'inv'), item);
-    const docSnapshot = await getDocs(docRefer);
+    const docSnapshot = await getDoc(docRefer);
     if (docSnapshot.exists()){
       const {quantity} = docSnapshot.data()
       if (quantity === 1)
@@ -67,7 +79,9 @@ export default function Home(){
       alignItems = {"center"} 
       justifyContent = {"center"} 
       display = {"flex"}
+      flexDirection={"column"}
       gap = {2}>
+        {/* <Typography variant="h1">Inventory Management</Typography> */}
         <Modal
           open = {open}
           onClose = {handleClose}>
@@ -99,12 +113,69 @@ export default function Home(){
                     sx={{
                       color: "black",
                       bgcolor: "white"
-                    }}>
-                  </TextField>
+                    }}/>
+
+                    <Button variant="contained"
+                      onClick={() => {
+                        addItem();
+                        setItemName('');
+                        handleClose();
+                      }}
+                      // sx={{
+                      //   color: "black",
+                      //   bgcolor: "white",
+                      // }}
+                      >
+                        Add</Button>
                 </Stack>
             </Box>
         </Modal>
-        <Typography variant="h1">Inventory Management</Typography>  
+        <Button variant="contained" onClick={()=>{handleOpen();}}>
+          Add Product
+        </Button>
+        <Box border="2px solid black">
+          <Box
+            width={800}
+            height={100}
+            bgcolor="black"
+            color="white"
+            display="flex"
+            alignItems="center"
+            justifyContent="center">
+              <Typography variant="h2" color="white">
+                Inventory Items
+              </Typography>
+          </Box>
+          <Stack width={800} height={300} spacing={2} overflow="auto">
+            {
+              inv.map(({name, quantity}) => (
+                <Box 
+                  key={name}
+                  width="100%"
+                  minHeight={150}
+                  display="flex"
+                  alignItems="center"
+                  justifyContent={"space-between"}
+                  padding={5}>
+                    <Typography variant="h3" justifyContent={"center"} alignItems={"center"} display={"flex"}>
+                      {name.charAt(0).toUpperCase() + name.slice(1)}
+                    </Typography>
+                    <Typography variant="h3" justifyContent={"center"} alignItems={"center"} display={"flex"}>
+                      {quantity}
+                    </Typography>
+                    <Stack direction={"row"} spacing={3}>
+                      <Button variant="contained" startIcon={<Add />} onClick={()=>{addItem(name);}}>
+                        Add
+                      </Button>
+                      <Button variant="contained" startIcon={<DeleteIcon />} onClick={()=>{removeItem(name);}}>
+                        Remove
+                      </Button>
+                    </Stack>
+                  </Box>
+              ))
+            }
+          </Stack>
+        </Box>
     </Box>
   );
 }
